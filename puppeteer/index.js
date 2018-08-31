1fc9aeebb818a953dafd66c1b02ed0db38dc990c
Luis Yucra <luis.yucra@gmail.com>
2018-08-30 18:17:07 -0700

    added first puppeteer script

---
+++ puppeteer/index.js
@@ -0,0 +1,38 @@
+const certName = 'oauth4';
+const puppeteer = require('puppeteer');
+const waitSpan = 2000;
+
+(async() => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://power-fun-2672-dev-ed.cs10.my.salesforce.com/secur/frontdoor.jsp?sid=00DJ0000003QQbr!ARgAQBXU8JJQQVn8zGCm3PqS8QSYeBlSXxM7kGPuf2fAPigLHrMsUen507pOj4Hz3V9XcS3DSg1kki_JX2oEIfhIlB5bv0kZ');
+  await page.waitForSelector('#setupLink');
+  await page.click('#setupLink');
+  await page.type('#setupSearch', 'cer');
+  await page.waitFor(waitSpan);
+  await page.waitForSelector('#CertificatesAndKeysManagement_font');
+  await page.click('#CertificatesAndKeysManagement_font');
+  console.log('00')
+  await page.waitFor(waitSpan);
+  await page.waitForSelector('input[name="new"]');
+  await page.click('input[name="new"]');
+  console.log('11')
+  await page.waitFor(waitSpan);
+  await page.waitForSelector('#MasterLabel');
+  await page.type('#MasterLabel', certName);
+  await page.click('#DeveloperName');
+  await page.click('input[name="save"]');
+  console.log('22')
+  await page.waitFor(waitSpan);
+  await page.waitFor(waitSpan);
+  await page._client.send('Page.setDownloadBehavior', {
+        behavior: 'allow',
+        downloadPath: './'
+    });
+  await page.waitForSelector('input[name="downloadcert"]');
+  await page.click('input[name="downloadcert"]');
+  await page.waitFor(waitSpan);
+  console.log("taking picture");
+  await page.screenshot({path: 'full.png', fullPage: true});
+  await browser.close();
+})();
